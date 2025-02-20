e237d2a9772753440952607af3e3b44c692ec543
dvk2807 <dovhyivolodymyr28@gmail.com>
2025-02-20 20:22:02 +0100

    Add post creation form

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
+from datetime import date as current_date
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<p>Hello, World!</p>"
+    return redirect("/post-creation")
+
+@app.route("/post-creation")
+def post_creation():
+    return render_template("post-creation.html")
+
+@app.route("/add-post", methods=["POST"])
+def create():
+    title = request.form.get("title", "Unnamed post")
+    date = request.form.get("date", current_date)
+    content = request.form.get("content", "No post content was provided.")
+    print(title, date, content, sep="\n")
+    
+    return redirect("/")
